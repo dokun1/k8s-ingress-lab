6724a6d1a152a211e95df2e623026f8bbac83586
David Okun <david@okun.io>
2019-05-12 19:27:02 -0400

    Adding http service for tls term demo

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,13 @@
 
 mkdir ingress-lab
 cd ingress-lab
+mkdir katacoda-resources
+cd katacoda-resources
 wget https://raw.githubusercontent.com/dokun1/k8s-ingress-lab/master/deployment.yaml
 wget https://raw.githubusercontent.com/dokun1/k8s-ingress-lab/master/ingress-rules.yaml
 wget https://raw.githubusercontent.com/dokun1/k8s-ingress-lab/master/ingress.yaml
 wget https://raw.githubusercontent.com/dokun1/k8s-ingress-lab/master/service.yaml 
+cd ..
+mkdir kube-resources
+cd kube-resources
+wget 
