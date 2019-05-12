5c8920b4b3217c48462e05468805190f9ff9f986
David Okun <david@okun.io>
2019-05-12 17:06:52 -0400

    Uploaded fleshed out shell script

--- prepare.sh
+++ prepare.sh
@@ -1,1 +1,8 @@
-wget 
+#!/bin/bash
+
+mkdir ingress-lab
+cd ingress-lab
+wget https://raw.githubusercontent.com/dokun1/k8s-ingress-lab/master/deployment.yaml
+wget https://raw.githubusercontent.com/dokun1/k8s-ingress-lab/master/ingress-rules.yaml
+wget https://raw.githubusercontent.com/dokun1/k8s-ingress-lab/master/ingress.yaml
+wget https://raw.githubusercontent.com/dokun1/k8s-ingress-lab/master/service.yaml 
